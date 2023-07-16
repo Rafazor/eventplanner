6b290b10abdb0d02f70a6a600474855d37d19155
Rafa Calin <rafa.calin@yahoo.com>
2023-07-16 22:27:28 +0300

    Fixed flex on nav

--- src/modules/layout/components/NavComponent.tsx
+++ src/modules/layout/components/NavComponent.tsx
@@ -5,7 +5,7 @@ import ThemeSwitch from "@/modules/layout/components/ThemeSwitch";
 export default function NavComponent() {
   return (
     <nav className="bg-purple-500 dark:bg-purple-950 fixed w-full z-20 top-0 left-0 border-b border-purple-700 dark:border-purple-950">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <div className="flex items-center">
           <Link href={"/"}>
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-purple-950 dark:text-purple-50">
@@ -13,7 +13,7 @@ export default function NavComponent() {
             </span>
           </Link>
         </div>
-        <div className="flex gap-10 items-center">
+        <div className="flex gap-5 items-center">
           <ThemeSwitch />
           <LinkButtonComponent href={"/manageEvents"} label={"Manage Events"} />
         </div>
